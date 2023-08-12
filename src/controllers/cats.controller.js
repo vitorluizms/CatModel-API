83f71e2bd1e06939df6214657ee0d2ddbd3fca95
vitorluizms <vitor.luiz.eer@gmail.com>
2023-08-12 10:52:55 -0300

    feat: Cadastro de gatos do usuário criado

--- src/controllers/cats.controller.js
+++ src/controllers/cats.controller.js
@@ -9,8 +9,6 @@ export const getCats = async (req, res) => {
                     GROUP BY cats.id, races.name
                     ORDER BY cats.id;`);
     res.status(200).send(cats.rows);
-    // INSERT INTO cats (name, "userId", age, color, race, description, size, "mainPic")
-    // VALUES ('gato de botas', 1, 12, 'preto', 1, 'gato que usa botas', 'Médio', 'dkjasdkjasd');
   } catch (err) {
     res.status.send(err.message);
   }
@@ -19,20 +17,46 @@ export const getCats = async (req, res) => {
 export const getCatById = async (req, res) => {
   const { id } = req.params;
   try {
-    const cat = await db.query(`SELECT cats.*, races.name AS "raceName",
-	CASE 
-    WHEN COUNT(pics) > 0 
-		THEN JSON_AGG(pics.url ORDER BY pics.id) 
-		ELSE '[]'::json
-		END AS pics
+    const cat = await db.query(
+      `SELECT cats.*, users.name AS user, races.name AS "raceName", 
+      pics.url AS pics
 	FROM cats
 	LEFT JOIN races ON races.id = cats.race
+  LEFT JOIN users ON users.id = cats."userId"
 	LEFT JOIN pics ON pics."catId" = cats.id
 	WHERE cats.id = $1
-	GROUP BY cats.id, races.name;
-    `, [id]);
+	GROUP BY cats.id, races.name, users.name, pics.url;
+    `,
+      [id]
+    );
     delete cat.rows[0].race;
+    delete cat.rows[0].userId;
+
     res.status(200).send(cat.rows[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+export const createCat = async (req, res) => {
+  const { userId } = res.locals.user;
+
+  const { name, age, color, race, description, size, mainPic, pics } = req.body;
+  try {
+    const promise = await db.query(
+      `INSERT INTO cats (name,"userId", age, color, race, description, size, "mainPic") 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id;
+    `,
+      [name, userId, age, color, race, description, size, mainPic]
+    );
+    if (pics.length === 0)
+      return res.status(201).send("Cadastro feito com sucesso!");
+
+    await db.query(`INSERT INTO pics (url, "catId") VALUES ($1, $2);`, [
+      pics,
+      promise.rows[0].id,
+    ]);
+    res.status(201).send("Cadastro com sucesso!");
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/routes/cats.routes.js
+++ src/routes/cats.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getCatById, getCats } from "../controllers/cats.controller.js";
+import {
+    createCat,
+    getCatById,
+    getCats,
+} from "../controllers/cats.controller.js";
+import validateAuth from "../middlewares/validateAuth.js";
 
 const catsRouter = Router();
 
 catsRouter.get("/cats", getCats);
 catsRouter.get("/cat/:id", getCatById);
+catsRouter.post("/cat", validateAuth, createCat);
 
 export default catsRouter;
